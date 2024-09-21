b5d6aa691201d4fdd2e4b08e0990b2519bd0ca4f
Bento Netto <bentoeemanuele@gmail.com>
2024-09-20 22:25:01 -0300

    Criação dos serviços de author

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
-
+import { AuthorModule } from './author/author.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthorModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/author/dto/create.author.dto.ts
@@ -0,0 +1,7 @@
+export class CreateAuthorDto {
+  name: string;
+  email: string;
+  bio: string;
+  cpf: string;
+  pais: string;
+  }
